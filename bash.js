cfd7acca76b07a6071dc2861dee266300732fa53
Isaac Easton <isaeaston@gmail.com>
2021-03-06 20:50:31 -0500

    in the middle of cat.js question

--- bash.js
+++ bash.js
@@ -2,6 +2,21 @@ process.stdout.write('prompt > ')
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim()
-  process.stdout.write('you typed: ' + cmd)
+  const pwd = require('./pwd.js')
+  const ls = require('./ls.js')
+  const cat = require('./cat.js')
+  if (cmd === 'pwd') {
+    pwd()
+  } else if (cmd === 'ls') {
+    process.stdout.write('directory files:\n' + ls)
+  } else if (cmd.includes(' ')) {
+    const cmdAndFile = cmd.split(' ')
+    if (cmdAndFile[0] === 'cat') {
+      process.stdout.write('File Contents:\n' + cat(cmdAndFile[1]))
+    }
+  } else {
+    process.stdout.write('you typed: ' + cmd)
+  }
   process.stdout.write('\nprompt > ')
 })
+
